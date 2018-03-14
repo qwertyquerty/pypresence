a201c517946bbab12ed0fb0f15e073f2bf147235
Isaac <qwertytrogi@gmail.com>
2018-03-13 21:26:34 -0500

    First commit

---
+++ examples/test.py
@@ -0,0 +1,9 @@
+import rpc
+client_id = '64567352374564' #Put your Client ID here
+RPC = rpc.DiscordRPC(client_id)
+RPC.start()
+
+print(RPC.set_activity(state="ree")) #Set the presence
+
+while True: #The presence will stay on as long as the program is running
+    pass
