574dce181fc5518a537bdf4e8302c223d81507cb
Isaac <qwertytrogi@gmail.com>
2018-03-14 10:42:50 -0500

    fixed the example because stupid mistakes

--- examples/test.py
+++ examples/test.py
@@ -1,6 +1,6 @@
-import rpc
+import pypresence
 client_id = '64567352374564' #Put your Client ID here
-RPC = rpc.client(client_id)
+RPC = pypresence.client(client_id)
 RPC.start()
 
 print(RPC.set_activity(state="ree")) #Set the presence
