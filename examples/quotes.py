8365be135c5a357c2b7de34be40eed79d1b70d05
Isaac <qwertytrogi@gmail.com>
2018-04-11 14:49:47 -0500

    Updated example 2

--- examples/quotes.py
+++ examples/quotes.py
@@ -6,6 +6,20 @@ RPC = pypresence.client(client_id) #Initialize the client class
 RPC.start() #Start the handshake loop
 
 
+quotes = [
+    "If you can dream it, you can achieve it.",
+    "Either write something worth reading or do something worth writing.",
+    "You become what you believe.",
+    "Fall seven times and stand up eight.",
+    "The best revenge is massive success.",
+    "Eighty percent of success is showing up.",
+    "Life is what happens to you while you’re busy making other plans.",
+    "Strive not to be a success, but rather to be of value.",
+    "The best time to plant a tree was 20 years ago. The second best time is now.",
+    "Everything you’ve ever wanted is on the other side of fear."
+] #The quotes to choose from
+
+
 while True: #The presence will stay on as long as the program is running
-    time.sleep(60) #Wait a wee bit
     print(RPC.set_activity(state="Click for quote!", details=random.choice(quotes))) #Set the presence, picking a random quote
+    time.sleep(60) #Wait a wee bit
