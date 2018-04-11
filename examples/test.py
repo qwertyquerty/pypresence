daf87f234c2508bd17c240bcdecc476baea0625a
Isaac <qwertytrogi@gmail.com>
2018-04-11 11:39:28 -0500

    Better Example

--- examples/test.py
+++
@@ -1,9 +0,0 @@
-import pypresence
-client_id = '64567352374564' #Put your Client ID here
-RPC = pypresence.client(client_id)
-RPC.start()
-
-print(RPC.set_activity(state="ree")) #Set the presence
-
-while True: #The presence will stay on as long as the program is running
-    pass
