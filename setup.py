3146a3055f67cce19d460e0c1266f8cfd2c9c500
qwerty <qwertytrogi@gmail.com>
2018-04-13 14:15:59 -0500

    Update

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(name='pypresence',
       version='1.0',
       packages=['pypresence'],
       license='MIT',
-      description='Discord RPC client written in python'
+      description='Discord RPC client written in python',
+	  long_description="A Discord RPC library in python! Wow! Looks like you've come to the right place. Just looking to do Rich Presence? We Got you covered."
 )
