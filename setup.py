a90ff2e20aa8ec1dbe14a6e1b635ecced9fa396a
Neko (Is Lewd) <nekoislewd@gmail.com>
2018-04-13 06:24:12 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pypresence',
+      author='qwertyquerty',
+      url='https://github.com/qwertyquerty/pypresence',
+      version='1.0',
+      packages=['pypresence'],
+      license='MIT',
+      description='Discord RPC client written in python'
+)
