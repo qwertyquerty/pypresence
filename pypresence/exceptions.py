2a92abce754b33d9dabf1428f7b8d4782ce14154
Isaac <qwertytrogi@gmail.com>
2018-04-20 12:42:09 -0500

    Update exceptions.py

--- pypresence/exceptions.py
+++ pypresence/exceptions.py
@@ -4,4 +4,4 @@ class InvalidID(Exception):
         
 class InvalidPipe(Exception):
     def __init__(self):
-        super().__init__('Pipe Not Found - Are you running a non-standard version of Discord?')
+        super().__init__('Pipe Not Found - Is Discord Running?')
