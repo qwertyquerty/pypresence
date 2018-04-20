4d21c3f7c2a1ddd30fdfa3cf43cf5ddb5f4bac30
Neko (Is Lewd) <nekoislewd@gmail.com>
2018-04-20 17:04:47 +0100

    Update rich-presence.py

--- examples/rich-presence.py
+++ examples/rich-presence.py
@@ -1,11 +1,11 @@
-from pypresence import Client
+from pypresence import Presence
 import time
 
 client_id = '64567352374564'  # Fake ID, put your real one here
-RPC = Client(client_id)  # Initialize the client class
-RPC.start() # Start the handshake loop
+RPC = Presence(client_id)  # Initialize the client class
+RPC.connect() # Start the handshake loop
 
-print(RPC.set_activity(state="Lookie Lookie", details="A test of qwertyquerty's Python Discord RPC wrapper, pypresence!"))  # Set the presence
+print(RPC.update(state="Lookie Lookie", details="A test of qwertyquerty's Python Discord RPC wrapper, pypresence!"))  # Set the presence
 
 while True:  # The presence will stay on as long as the program is running
     time.sleep(15) # Can only update rich presence every 15 seconds
