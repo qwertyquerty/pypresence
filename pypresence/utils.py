0db925209f2964e83fa1a03dac5455fc52e93de9
Neko (Is Lewd) <nekoislewd@gmail.com>
2018-04-20 17:19:24 +0100

    Update utils.py

--- pypresence/utils.py
+++ pypresence/utils.py
@@ -3,7 +3,7 @@ def remove_none(d: dict): # Made by https://github.com/LewdNeko ;^)
     for item in d.copy():
         if isinstance(d[item], dict):
             if len(d[item]):
-                d[item] = none_remover(d[item])
+                d[item] = remove_none(d[item])
             else:
                 del d[item]
         elif d[item] is None:
