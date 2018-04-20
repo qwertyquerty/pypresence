e7c6efa9b4b64c356c9be273ee9cc2fc95af2f2c
Neko (Is Lewd) <nekoislewd@gmail.com>
2018-04-20 16:40:28 +0100

    Create utils.py

---
+++ pypresence/utils.py
@@ -0,0 +1,11 @@
+# Util functions that are needed but messy.
+def remove_none(d: dict):
+    for item in d.copy():
+        if isinstance(d[item], dict):
+            if len(d[item]):
+                d[item] = none_remover(d[item])
+            else:
+                del d[item]
+        elif d[item] is None:
+            del d[item]
+    return d
