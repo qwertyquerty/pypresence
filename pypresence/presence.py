50d07992074ddc684ea06cc50f750f6ee7dddd45
Neko (Is Lewd) <nekoislewd@gmail.com>
2018-04-20 16:06:18 +0100

    Update presence.py

--- pypresence/presence.py
+++ pypresence/presence.py
@@ -2,13 +2,13 @@ from .client import Client
 
 class Presence(Client):
     def __init__(self, **kwargs):
-        self.super().__init__(**kwargs)
+        super().__init__(**kwargs)
         
     def connect():
-        self.super().start()
+        super().start()
         
     def update(**kwargs):
-        self.super().set_activity(**kwargs)
+        super().set_activity(**kwargs)
         
     def close():
-        self.super().close()
+        super().close()
