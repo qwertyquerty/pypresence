814527246d67f526b041d978c5f360ca389589ff
qwerty <qwertytrogi@gmail.com>
2018-04-27 16:35:48 -0500

    1.0.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pypresence',
       author='qwertyquerty',
       url='https://github.com/qwertyquerty/pypresence',
-      version='1.1',
+      version='1.0.7',
       packages=['pypresence'],
       license='MIT',
       description='Discord RPC client written in python',
