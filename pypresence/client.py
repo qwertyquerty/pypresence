5ef15533d714c8011c2c24cb16c8318463626c2f
Isaac <qwertytrogi@gmail.com>
2018-05-01 10:26:51 -0500

    added clear_activity

--- pypresence/client.py
+++ pypresence/client.py
@@ -236,6 +236,19 @@ class Client:
         sent = self.send_data(1, payload)
         return self.loop.run_until_complete(self.read_output())
 
+    def clear_activity(self,pid=os.getpid()):
+        current_time = time.time()
+        payload = {
+            "cmd": "SET_ACTIVITY",
+            "args": {
+                "pid": pid,
+                "activity": None
+            },
+            "nonce": '{:.20f}'.format(current_time)
+        }
+        self.send_data(1, payload)
+        return self.loop.run_until_complete(self.read_output())
+    
     def subscribe(self,event,args={}):
         current_time = time.time()
         payload = {
