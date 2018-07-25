9d5f6d0a99089b2b622b8787540a2b59c6e4eb44
Neko (Is Lewd) <nekoislewd@gmail.com>
2018-07-25 09:57:45 +0100

    Update exceptions.py

--- pypresence/exceptions.py
+++ pypresence/exceptions.py
@@ -1,30 +1,34 @@
-class InvalidID(Exception):
+class PyPresenceException(Exception):
+    def __init__(self, message=None):
+        message = 'An error has occured within PyPresence' if message is None
+        super().__init__(message)
+
+class InvalidID(PyPresenceException):
     def __init__(self):
         super().__init__('Client ID is Invalid')
 
 
-class InvalidPipe(Exception):
+class InvalidPipe(PyPresenceException):
     def __init__(self):
         super().__init__('Pipe Not Found - Is Discord Running?')
 
 
-class ServerError(Exception):
+class ServerError(PyPresenceException):
     def __init__(self, message):
         super().__init__(message.replace(']','').replace('[','').capitalize())
 
 
-class DiscordError(Exception):
+class DiscordError(PyPresenceException):
     def __init__(self, code, message):
         self.code = code
         self.message = message
         super().__init__('Error Code: {0} Message: {1}'.format(code, message))
 
-
-class ArgumentError(Exception):
+class ArgumentError(PyPresenceException):
     def __init__(self):
-        super().__init__('Event function must have one argument.')
+        super().__init__('Supplied function must have one argument.')
 
 
-class EventNotFound(Exception):
+class EventNotFound(PyPresenceException):
     def __init__(self, event):
         super().__init__('No event with name {0} exists.'.format(event))
