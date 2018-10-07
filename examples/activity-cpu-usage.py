a96f7b3607230fd403e043bb2b6927a39d0bc8b3
qwerty <qwertytrogi@gmail.com>
2018-10-06 21:58:12 -0500

    fixed zample

--- examples/activity-cpu-usage.py
+++ examples/activity-cpu-usage.py
@@ -9,6 +9,8 @@ RPC = Presence(client_id)  # Initialize the client class
 RPC.connect() # Start the handshake loop
 ac = Activity(RPC)
 
+ac.start = int(time.time())
+
 while True:  # The presence will stay on as long as the program is running
     cpu_per = round(psutil.cpu_percent(),1) # Get CPU Usage
     mem_per = round(psutil.virtual_memory().percent,1) #Get Mem Usage
