4cef016ca12b576411d341d4c2a6509099e22b12
Neko <24483290+LewdNeko@users.noreply.github.com>
2018-10-20 03:43:35 +0100

    Update and rename images.py to rich-presence-images.py

--- examples/rich-presence-images.py
+++ examples/rich-presence-images.py
@@ -1,12 +1,11 @@
 from pypresence import Presence
-
 """
 You need to upload your image(s) here:
 https://discordapp.com/developers/applications/<APP ID>/rich-presence/assets
 """
 
-client_id = 64567352374534
-RPC = pypresence.Presence(client_id=client_id, pipe=0)
+client_id = 12345678910  # Enter your Application ID here.
+RPC = pypresence.Presence(client_id=client_id)
 RPC.connect()
 
 # Make sure you are using the same name that you used when uploading the image
