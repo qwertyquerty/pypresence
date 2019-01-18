226eb89001267c1e8702d9cd82762108a4ecdfb6
Neko <24483290+LewdNeko@users.noreply.github.com>
2019-01-18 18:42:26 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(name='pypresence',
       author='qwertyquerty',
       url='https://github.com/qwertyquerty/pypresence',
-      version='3.3.0',
+      version='3.3.1',
       packages=['pypresence'],
       python_requires='>=3.5',
       platforms=['Windows', 'Linux', 'OSX'],
