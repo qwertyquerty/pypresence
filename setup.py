98690a02fa7197046a6808d82db0bf8d4bb0db05
qwerty <qwertytrogi@gmail.com>
2021-01-21 01:52:41 -0600

    4.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 setup(name='pypresence',
       author='qwertyquerty',
       url='https://github.com/qwertyquerty/pypresence',
-      version='4.1.0',
+      version='4.1.1',
       packages=['pypresence'],
       python_requires='>=3.5',
       platforms=['Windows', 'Linux', 'OSX'],
