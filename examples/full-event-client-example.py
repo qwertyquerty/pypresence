d455d0e2cace649ceadcbd0c8a976e63f0dcb001
FailSpy <failspy@exonull.com>
2021-03-25 08:54:04 +0000

    Cleanup and whitespace

--- examples/full-event-client-example.py
+++ examples/full-event-client-example.py
@@ -3,15 +3,14 @@ import pypresence
 
 CLIENT_ID = 555555555555555555
 
-def message_created(data):
-    print("New message: %s" % data['message']['content'])
-
 loop = asyncio.get_event_loop()
-c = pypresence.Client(CLIENT_ID,loop=loop)
+c = pypresence.Client(CLIENT_ID, loop=loop)
 c.start()
-# Prompt user for authorization to do stuff
-auth = c.authorize(CLIENT_ID,['rpc'])
+
+# Prompt user for authorization to do stuff with RPC scopes
+auth = c.authorize(CLIENT_ID, ['rpc'])
 code_grant = auth['data']['code']
+
 '''
 Implement the API found here for code/token exchange: 
 https://discord.com/developers/docs/topics/oauth2#authorization-code-grant-access-token-exchange-example
@@ -22,8 +21,11 @@ token = exchange_code(code_grant)
 # Now authenticate with the token we got (Save to skip authorization later)
 c.authenticate(token['access_token'])
 
+def on_new_message(data):
+    print("New message: %s" % data['message']['content'])
+
 # Watch for new messages created on channel 444444444444444444
-a = c.register_event('MESSAGE_CREATE',message_created,args={'channel_id':'444444444444444444'))
+c.register_event('MESSAGE_CREATE', on_new_message, args={'channel_id':'444444444444444444'))
 # Find other event types here: https://discord.com/developers/docs/topics/rpc#commands-and-events-rpc-events
 
 loop.run_forever()
