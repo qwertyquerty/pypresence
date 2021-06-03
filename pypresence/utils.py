9aab515b2962be53bd7b68ad9eb9fef66aa0d9e6
Neko <nekoislewd@gmail.com>
2021-06-03 22:21:50 +0100

    Remove old, eventually scrapped code

--- pypresence/utils.py
+++ pypresence/utils.py
@@ -44,31 +44,6 @@ def get_ipc_path(pipe=None):
     return None
 
 
-# Don't call these. Ever.
-def _load_payloads(filename):
-    with open(filename, 'r') as fp:
-        f = fp.read()
-
-    payloaddict = {}
-    for line in f.splitlines():
-        name, payload = line.split('||')
-        payloaddict[name] = payload
-
-    return payloaddict
-
-
-def _payload_gen(payload_type: str, payload_params: dict):
-    payloads = _load_payloads('pllist.NEKO')  # dont like txt files
-    if payload_type.upper() not in payloads:
-        raise PyPresenceException('Payload type not supported or does not exist.')
-    payload_str = payloads[payload_type]
-    for key, value in payload_params.items():
-        payload_str.replace(";;{0};;".format(key), value)
-
-    payload = json.loads(payload_str)
-    payload["nonce"] = payload["nonce"].format(time.time())
-
-
 # This code used to do something. I don't know what, though.
 try:  # Thanks, Rapptz :^)
     create_task = asyncio.ensure_future
