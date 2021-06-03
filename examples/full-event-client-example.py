86cd7f64f3d4b5fddf598962e445cb900680dd7b
Neko <nekoislewd@gmail.com>
2021-06-03 22:34:17 +0100

    Fix some issues with the new example

--- examples/full-event-client-example.py
+++ examples/full-event-client-example.py
@@ -8,7 +8,7 @@ c = pypresence.Client(CLIENT_ID, loop=loop)
 c.start()
 
 # Prompt user for authorization to do stuff with RPC scopes
-auth = c.authorize(CLIENT_ID, ['rpc'])
+auth = c.authorize(str(CLIENT_ID), ['rpc'])
 code_grant = auth['data']['code']
 
 '''
@@ -21,11 +21,13 @@ token = exchange_code(code_grant)
 # Now authenticate with the token we got (Save to skip authorization later)
 c.authenticate(token['access_token'])
 
+
 def on_new_message(data):
     print("New message: %s" % data['message']['content'])
 
+
 # Watch for new messages created on channel 444444444444444444
-c.register_event('MESSAGE_CREATE', on_new_message, args={'channel_id':'444444444444444444'))
+c.register_event('MESSAGE_CREATE', on_new_message, args={'channel_id': '444444444444444444'})
 # Find other event types here: https://discord.com/developers/docs/topics/rpc#commands-and-events-rpc-events
 
 loop.run_forever()
