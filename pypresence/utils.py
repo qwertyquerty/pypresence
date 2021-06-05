10e31137c5cb9fcc75ad11027ad12e395dc9d6c4
failspy <failspy@exonull.com>
2021-06-04 23:36:09 -0400

    Build out PartialUser model

--- pypresence/utils.py
+++ pypresence/utils.py
@@ -12,12 +12,66 @@ from .exceptions import PyPresenceException
 PremiumType = Enum('PremiumType', {'None': 0, 'Nitro Classic': 1, 'Nitro': 2})
 
 
-class UserFlags:
-    pass
+class UserFlags(Enum):
+    # shamelessly ripped from discord.py's enums.py
+    staff = 1
+    partner = 2
+    hypesquad = 4
+    bug_hunter = 8
+    mfa_sms = 16
+    premium_promo_dismissed = 32
+    hypesquad_bravery = 64
+    hypesquad_brilliance = 128
+    hypesquad_balance = 256
+    early_supporter = 512
+    team_user = 1024
+    system = 4096
+    has_unread_urgent_messages = 8192
+    bug_hunter_level_2 = 16384
+    verified_bot = 65536
+    verified_bot_developer = 131072
+    discord_certified_moderator = 262144
 
 
 class PartialUser:
-    pass
+    def __init__(self, data):
+        self._update(data)
+
+    def _update(self, data):
+        self._id = data.get('id')
+        self._username = data.get('username')
+        self._discriminator = data.get('discriminator')
+        avatar = data.get('avatar')
+        self._avatar = f'https://cdn.discordapp.com/avatars/{self._id}/{avatar}' if avatar else f'https://cdn.discordapp.com/embed/avatars/{self._discriminator % 5}.png'
+        self._flags = data.get('flags',0)
+        self._premium_type = PremiumType(data.get('premium_type',0))
+
+    @property
+    def id(self):
+        return self._id
+
+    @property
+    def username(self):
+        return self._username
+
+    @property
+    def discriminator(self):
+        return self._discriminator
+
+    @property
+    def avatar(self):
+        return self._avatar
+
+    @property
+    def premium_type(self):
+        return self._premium_type
+
+    @property
+    def flags(self):
+        return [flag for flag in UserFlags if self.has_flag(flag)]
+
+    def has_flag(self, flag: UserFlags):
+        return (self._flags & flag.value) == flag.value
 
 
 # Made by https://github.com/LewdNeko ;^)
