708ba92ff96c0b738fac8a725c7ae47b5614e255
Neko <24483290+LewdNeko@users.noreply.github.com>
2021-11-15 17:03:25 +0000

    Create rich-presence-get-request.py

---
+++ examples/rich-presence-get-request.py
@@ -0,0 +1,25 @@
+import time
+
+import requests  # Needs requests module installed: pip install requests -U
+from pypresence import Presence
+
+
+CLIENT_ID = ''  # Your client ID here
+
+
+def get_presence_data():
+    with requests.get('https://somewebsite.com/api/status') as resp:
+        data = resp.json()
+    # Use the data in whatever way you want, and return kwargs for the Presence.update() method
+    return {'state': data['online'].title(), 'details': 'SomeWebsite Status', 'start': data['start']}
+
+def run():
+    presence = Presence(CLIENT_ID)
+    presence.connect()
+    while True:
+        data = get_presence_data()
+        presence.update(**data)
+        time.sleep(15)
+        
+if __name__ in '__main__':
+    run()
