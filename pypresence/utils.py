68d1e936f47b1eb393672b1257d7be9c3eca465e
brostos <67740566+brostosjoined@users.noreply.github.com>
2023-03-06 03:53:20 +0300

    Update utils.py

--- pypresence/utils.py
+++ pypresence/utils.py
@@ -59,12 +59,9 @@ def get_event_loop(force_fresh=False):
     elif sys.platform == 'win32':
         if force_fresh:
             return asyncio.ProactorEventLoop()
-        try:
-            loop = asyncio.get_event_loop()
-        except RuntimeError:
-            return asyncio.ProactorEventLoop()
+        loop = asyncio.get_event_loop()
         if isinstance(loop, asyncio.ProactorEventLoop) and not loop.is_closed():
-            return asyncio.ProactorEventLoop()
+            return loop
         return loop
 
 
