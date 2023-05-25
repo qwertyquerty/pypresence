4069432fe8206a67f7ad738aabcbbddab6f56afa
L. de Kimpe <electrenator2000@protonmail.com>
2023-05-25 14:42:50 +0200

    Allow running without env variable on Linux
    Minimalized improvement of commit Electrenator/pypresence@4030089605d41e7c87673ed10519f7f8023b1853 with  the path variables untouched.
    
    Resolves qwertyquerty/pypresence#216

--- pypresence/utils.py
+++ pypresence/utils.py
@@ -29,8 +29,19 @@ def get_ipc_path(pipe=None):
         ipc = f"{ipc}{pipe}"
 
     if sys.platform in ('linux', 'darwin'):
-        tempdir = (os.environ.get('XDG_RUNTIME_DIR') or tempfile.gettempdir())
+        tempdir = os.environ.get('XDG_RUNTIME_DIR') # Runtime dir set by Linux GUI environment
         paths = ['.', 'snap.discord', 'app/com.discordapp.Discord', 'app/com.discordapp.DiscordCanary']
+        users_runtime_dir = f"/run/user/{os.getuid()}" # Possible location for Linux user's runtime
+
+        if tempdir:
+            pass # No overwrite needed, can probably trust the set XDG_RUNTIME_DIR variable
+        elif os.path.exists(users_runtime_dir):
+            # Runtime directory check as fix for #216
+            tempdir = users_runtime_dir
+        else:
+            tempdir = tempfile.gettempdir()
+
+        del users_runtime_dir
     elif sys.platform == 'win32':
         tempdir = r'\\?\pipe'
         paths = ['.']
