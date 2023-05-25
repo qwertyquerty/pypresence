cbc04f6bba8dd3e7d26b3554f7a1771852e390c5
Cat <24483290+TheSpookyCat@users.noreply.github.com>
2023-05-25 18:04:47 +0100

    Update rich-presence-gifs.py
    Replace with non-meme

--- examples/rich-presence-gifs.py
+++ examples/rich-presence-gifs.py
@@ -13,7 +13,7 @@ RPC = Presence(client_id=client_id)
 RPC.connect()
 
 
-RPC.update(state="Custom gif's",details="Using pypresence",large_image=("https://media.tenor.com/uAqNn6fv7x4AAAAM/bombsquad-spaz.gif"), large_text="Dancing Spazito!",
+RPC.update(state="Custom gifs",details="Using pypresence",large_image=("https://media.tenor.com/9c83g4VPctQAAAAi/shaking-eyes-eyes-shaking.gif"), large_text="Watching!",
             small_image=("https://media.giphy.com/media/4UjV5LeD66EPruSG18/giphy.gif"), small_text="Spinning")
 while True:
     time.sleep(1)
