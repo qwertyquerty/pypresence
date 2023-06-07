cfd990fa5d5ac20e2c55a586b78e3e126bc7232b
andres23_007 <106448661+andres23-007@users.noreply.github.com>
2023-06-07 21:54:40 +0200

    cpu_stat+time+image.py

--- examples/cpu_stat+time_immage - Copy.py
+++ examples/cpu_stat+time_immage - Copy.py
@@ -1,9 +1,8 @@
-# iport requirements
 import psutil
 from pypresence import Presence
 import time
 
-client_id = 'EWEEEEEEEEEEEE'  # Fake ID, put your real one here
+client_id = '9999999999999999999999999999999999 '  # Fake ID, put your real one here
 RPC = Presence(client_id,pipe=0)  # Initialize the client class
 RPC.connect() # Start the handshake loop
 
@@ -11,12 +10,9 @@ start_time=time.time() #starts the time countdown
 
 while True:  # The presence will stay on as long as the program is running
     cpu_per = round(psutil.cpu_percent(),1) # Get CPU Usage
-
     mem = psutil.virtual_memory() # get memory
     mem_per = round(psutil.virtual_memory().percent,1)
-
     print(RPC.update(details="RAM: "+str(mem_per)+"%", state="CPU: "+str(cpu_per)+"%",
-    large_image="existing",
+    large_image="YOUR IMAGE NAME",
     start=start_time))  # Set the presence
-    
     time.sleep(15) # Can only update rich presence every 15 seconds
