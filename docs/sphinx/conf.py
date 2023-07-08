015f811451bf1ce7825afb4a79a01933d3b4e272
madeline <qwertytrogi@gmail.com>
2023-07-07 19:47:15 -0500

    update docs

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -27,9 +27,9 @@ author = 'qwertyquerty, LewdNeko'
 
 # The short X.Y version
 
-version = '3.2'
+version = '4.3'
 # The full version, including alpha/beta/rc tags
-release = '3.2.0'
+release = '4.3.0'
 
 # -- General configuration ---------------------------------------------------
 
