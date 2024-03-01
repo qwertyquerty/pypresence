530e799719328b0707260ebb53804140af842a96
NikOverflow <nikoverflow007@gmail.com>
2024-03-01 17:13:34 +0100

    fix: fix windows error if the ipc file can't be used.
    Signed-off-by: NikOverflow <nikoverflow007@gmail.com>

--- pypresence/utils.py
+++ pypresence/utils.py
@@ -21,6 +21,15 @@ def remove_none(d: dict):
     return d
 
 
+def test_ipc_path(path):
+    '''Tests an IPC pipe to ensure that it actually works'''
+    try:
+        with open(path):
+            return True
+    except Exception:
+        return False
+
+
 # Returns on first IPC pipe matching Discord's
 def get_ipc_path(pipe=None):
     ipc = 'discord-ipc-'
@@ -40,7 +49,7 @@ def get_ipc_path(pipe=None):
         full_path = os.path.abspath(os.path.join(tempdir, path))
         if sys.platform == 'win32' or os.path.isdir(full_path):
             for entry in os.scandir(full_path):
-                if entry.name.startswith(ipc) and os.path.exists(entry):
+                if entry.name.startswith(ipc) and os.path.exists(entry) and test_ipc_path(entry):
                     return entry.path
 
 
