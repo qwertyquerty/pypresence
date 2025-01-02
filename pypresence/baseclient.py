cd4b47811ebf0ff7ad097a35afa7a278fe2dbfdc
Niklas S. <nikoverflow007@gmail.com>
2025-01-02 20:52:44 +0100

    Added #242

--- pypresence/baseclient.py
+++ pypresence/baseclient.py
@@ -51,7 +51,7 @@ class BaseClient:
             else:
                 err_handler = self._err_handle
 
-            loop.set_exception_handler(err_handler)
+            self.loop.set_exception_handler(err_handler)
             self.handler = handler
 
         if getattr(self, "on_event", None):  # Tasty bad code ;^)
